58f506a51f513dcb2bc2602489da92e93c965d71
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-05-31 13:32:44 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StarterHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Nice to meet you " + name);
+            int maxInt = int.MaxValue;
+            Console.WriteLine("Max Int: " + maxInt);
+        }
+    }
+}
